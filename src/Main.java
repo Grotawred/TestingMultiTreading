57d62f361c14cc507558ab80f6d336472d70fc6f
Grotawred <grishavlasko200@gmail.com>
2022-07-20 23:30:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Main thread started...");
+//        for(int i=1; i < 6; i++)
+//            new TestTread("TestThread " + i).start();
+//        System.out.println("Main thread finished...");
+//        _________________________________________________
+//        System.out.println("Main thread started...");
+//        Thread myThread = new Thread(new MyThreads(),"MyThread");
+//        myThread.start();
+//        System.out.println("Main thread finished...");
+//        ________________________________________________
+//        System.out.println("Main thread started...");
+//        TestTread t= new TestTread("TestThread ");
+//        t.start();
+//        try{
+//            t.join();
+//        }
+//        catch(InterruptedException e){
+//
+//            System.out.printf("%s has been interrupted", t.getName());
+//        }
+//        System.out.println("Main thread finished...");
+//        _________________________________________________
+//        System.out.println("Main thread started...");
+//        Runnable r = ()->{
+//            System.out.printf("%s started... \n", Thread.currentThread().getName());
+//            try{
+//                Thread.sleep(500);
+//            }
+//            catch(InterruptedException e){
+//                System.out.println("Thread has been interrupted");
+//            }
+//            System.out.printf("%s finished... \n", Thread.currentThread().getName());
+//        };
+//        Thread myThread = new Thread(r,"MyThread");
+//        myThread.start();
+//        System.out.println("Main thread finished...");
+    }
+}
---
+++ src/MyThreads.java
@@ -0,0 +1,13 @@
+public class MyThreads implements Runnable{
+    @Override
+    public void run() {
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try{
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s finished... \n", Thread.currentThread().getName());
+    }
+}
